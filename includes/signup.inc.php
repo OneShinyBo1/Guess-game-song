b75cedd1b0593aa10c633bd66b4cf71e36bb6300
August Bakker <august.abel.vian.bakker@gmail.com>
2023-06-11 20:54:07 +0200

    Sign Up basically done

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -10,7 +10,7 @@ if (isset($_POST["submit"])){
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
-    
+
     if (emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false){
         header("location: ../signup.php?error=emptyinput");
         exit();
@@ -32,6 +32,7 @@ if (isset($_POST["submit"])){
         exit();
     }
 
+    createUser($conn, $name, $email, $username, $pwd);
 
 
 }else{
